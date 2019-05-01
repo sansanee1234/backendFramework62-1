cc0bc88e2790634ed5be2225829b940bdc7db0b5
sansanee1234 <niroj2838@gmail.com>
2019-05-01 11:23:18 -0700

    Set port.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(function (req, res, next) {
 });
 
 //const port = 3000
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Running on http://localhost:${port}`);
 });
